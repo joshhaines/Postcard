8e0beafd335ecae93b77355b65a7cdd8ef6e22e6
personary <apersonary@gmail.com>
2015-01-19 16:42:33 -0500

    first challenge additions

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,14 +28,27 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Adding a comment here to test commit
+        
+        // Change messageLabel text to include entered text from enterMessageTextField
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        // Set enterMessageTextField text back to blank and dismiss the keyboard
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        // Set the button title to "Mail Sent"
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        // Change nameLabel text to include entered text from enterNameTextField
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        // Set enterNameTextField text back to blank and dismiss the keyboard
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
     }
 
 }
